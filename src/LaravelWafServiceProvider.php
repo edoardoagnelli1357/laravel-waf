37e1f07ee50e3b967742cbb0004e9f8c1b628d1b
fijan <c0deb100d.2001@gmail.com>
2025-12-09 15:50:57 +0330

    Create package.

---
+++ src/LaravelWafServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Edoardoagnelli1357\LaravelWaf;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelWafServiceProvider extends ServiceProvider{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
